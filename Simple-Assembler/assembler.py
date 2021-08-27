45b3469a8debea5ec77b0bdfcfa1131687415b47
Praveen Singh Samota <88671507+praveensamota@users.noreply.github.com>
2021-08-27 20:51:32 +0530

    Update assembler.py

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -1,15 +1,19 @@
 #!/bin/bash
 from sys import stdin
+
 a=[]
+x=0
+y=0
 linenum=0
 temp_var=[]
-dict_var={} #dict_var is a dictionary which takes key as variable names and value as addresses of variable.
+dict_var={} 
 address=0
+allowed_chars=set(("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"))
 halt=False
 endvar=False
-kw = ['add','sub','mul','xor','or','and','rs','ls','mov','div','not','cmp','ld','st','jmp','jlt','jgt','je','hlt'] #list of keywords we've to use in the program accordingly
+kw = ['add','sub','mul','xor','or','and','rs','ls','mov','div','not','cmp','ld','st','jmp','jlt','jgt','je','hlt']
 
-lable_dict ={} #label_dic is a dictionary which takes key as lable names and value as addresses of lable.
+lable_dict ={}
 
     
 
@@ -21,10 +25,14 @@ for line in stdin:
     stripped_line=line.strip()
     line_list=stripped_line.split()
     a.append(line_list)
-    def label(o): # This function is a global function and is used to fetch the label and their addresses.
+    if len(line_list)==0:
+        continue
+    def label(o):
         
+            
         for i in range(0,len(o)):
-            if o[0][0]=='var':
+    
+            if o[i][0]=='var':
                 o.remove(o[0])
             else:
                 break
@@ -34,7 +42,7 @@ for line in stdin:
                 addr='{0:08b}'.format(i)
                 lable_dict.update({label:addr})
         return lable_dict
-    label(a)
+    
     if halt==True:
         print(str(linenum)+ ": Syntax Error: hlt instuction must be given in the end.")
         quit()
@@ -44,17 +52,37 @@ for line in stdin:
         quit()
 
     if line_list[0]=='var':
-        if line_list[1] in temp_var:
+        if len(line_list)==1:
+            print(str(linenum)+ ": Wrong instruction: variable name not defined")
+            quit()
+        
+        characters=set((line_list[1]))
+        if characters.issubset(allowed_chars):
+            x=1
+
+        if x!=1:
+            print(str(linenum) + ": Invalid Variable Name")    
+            quit()
+            
+
+        if line_list[1]=='var':
+            print(str(linenum) + ": Variable name cannot be 'var'")
+            quit()
+
+        elif line_list[1] in temp_var:
             print(str(linenum) + ": Syntax Error: 2 or more variables cannot have the same name.")
             quit()
         
-        if line_list[1] in lable_dict.keys():
+        elif line_list[1] in lable_dict.keys():
             print(str(linenum) + ": Syntax Error: variables and labels can't have same name")
             quit()
 
         elif line_list[1] in kw:
             print(str(linenum) + ":Syntax Error: Instruction Keywords can't be used as variables")
             quit()
+
+        
+        
 
         else:
             temp_var.append(line_list[1])
@@ -63,6 +91,17 @@ for line in stdin:
     elif line_list[0][-1]==':':
         endvar=True
 
+        if len(line_list)==1:
+            print(str(linenum)+ ": Wrong instruction: label name not defined")
+            quit()
+
+        labelchar=set((line_list[0][:-1]))
+        if labelchar.issubset(allowed_chars):
+            y=1
+
+        if y!=1:
+            print(str(linenum) + ": Invalid Label Name")    
+            quit()
         if line_list[0][:-1] in temp_var:
             print(str(linenum)+ ":Syntax Error: Labels and variables can't have same name")
             quit()
@@ -71,6 +110,8 @@ for line in stdin:
             print(str(linenum)+ ":Syntax Error: Instruction keywords can't be used as labels")
             quit()
         
+   
+
         else:
             label(a)
         if line_list[1]=='hlt':
@@ -80,6 +121,7 @@ for line in stdin:
     
     else:
         if line_list[0]=='hlt':
+
             endvar=True
             halt=True
         endvar=True
@@ -103,9 +145,10 @@ if(address>257):
 
 
 
-for i in range (0,len(a)): 
-
-    def registers(k): # This function returns a list of binary numbers accordance to the registers used.Takes one parameter.
+for i in range (0,len(a)):
+    if len(a[i])==0:
+        continue 
+    def registers(k):
         reg =[]
         var = k
         for j in range(1,len(a[i])):
@@ -127,7 +170,7 @@ for i in range (0,len(a)):
                 reg.append("111")
         return reg
 
-    def opcode(word):  # This is a kind of helper function which has a opcode dictionary embedded in it with key as the parameter word and value as opcode.Takes only one parameter.
+    def opcode(word):
         opc={"add":"00000",
              "sub":"00001",
              "mov":tuple(["00010","00011"]),
@@ -159,8 +202,7 @@ for i in range (0,len(a)):
             x=opc.get(word)
 
         return x
-        
-    def typeA(g): # This function is made exclusively for creating a type A machine code.
+    def typeA(g):
 
         if a[i][0][-1]==':':
             if((g[2][0]=='R' and g[3][0]=='R' and g[4][0]=='R')==0):
@@ -185,7 +227,7 @@ for i in range (0,len(a)):
  
     
     
-    def typeB(t):  # This function is made exclusively for creating a type B machine code.
+    def typeB(t):
         if((t[1][0]=='R' and t[1][1].isdecimal() and t[2][0]=='$' and t[2][1:].isdecimal())==0):
             print("Invalid Syntax: Type B can't be interpreted in this way")
             quit()
@@ -193,7 +235,7 @@ for i in range (0,len(a)):
         if(t[2][0]=='$'):
             x=int(t[2][1:])
             if x<0 or x>255:
-                print("Syntax: Immediate Value out of limits(0-255)")
+                print(str(i) + " : Immediate Value out of limits(0-255)")
                 quit()
         z=opcode(t[0])
         y=registers(t)
@@ -202,7 +244,7 @@ for i in range (0,len(a)):
         code=z+y[0]+str(imm)
         print(code)
    
-    def typeC(m):  # This function is made exclusively for creating a type C machine code.
+    def typeC(m):  
         if(m[1][0]=='R' and m[1][1].isdecimal() and ((m[2][0]=='R' and m[2][1].isdecimal())  or m[2]=='FLAGS')==0):
            print("Invalid Syntax: Type C can't be interpreted in this way")
            quit()
@@ -211,13 +253,13 @@ for i in range (0,len(a)):
         code=z+'00000'+y[0]+y[1]
         print(code)
     
-    def typeD(p):  # This function is made exclusively for creating a type D machine code.
+    def typeD(p):
         if((p[1][0]=='R' and p[1][1].isdecimal())==0):
             print("Invalid Syntax: Type D can't be interpreted in this way")
             quit()
 
         if(p[2] not in dict_var.keys()):
-            print("Variable" +" "+ p[2]+ " "  + "not found")
+            print(str(i+1)+" "+": Variable" +" "+ p[2]+ " "  + "not found")
             quit()
         z=opcode(p[0])
         y=registers(p)
@@ -226,19 +268,30 @@ for i in range (0,len(a)):
         add1 ='{0:08b}'.format(li)
         
         code = z+y[0]+ str(add1)
+
+        
         print(code)
         
         
 
-    def typeE(q):  # This function is made exclusively for creating a type E machine code.
+    def typeE(q):
         if q[0]=='jmp' or q[0]=='jlt' or q[0]=='jgt' or q[0]=='je':
             z=opcode(q[0])
+            
+
 
         x=label(a)
-        code=z+"000"+x.get(q[1])
+        if not x:
+            print(str(i+1)+ ": Label not found")
+            quit()
+        else :    
+            code=z+"000"+x.get(q[1])
+        
         print(code)
     
-    def typeF(n): # This function is made exclusively for creating a type F machine code.
+
+
+    def typeF(n):
         if a[i][0][-1]==':':
             z=opcode(n[1])
         else:
@@ -247,16 +300,30 @@ for i in range (0,len(a)):
         print(code)
     
 
-#below if-else statements are used to call the respective fuctions accordingly 
 
+
+    if a[i][0] not in kw:
+        if a[i][0]!="var":
+            if a[i][0][-1]!=":":
+                if a[i][0]!="hlt":
+                    print(str(i+1)+ ": given instruction is invalid")
+                    quit()
     if a[i][0]=='add' or a[i][0]=='sub' or a[i][0]=='mul' or a[i][0]=='xor' or a[i][0]=='or' or a[i][0]=='and':
         typeA(a[i])
     
 
-    if a[i][0]=='mov' and a[i][1][0]=='R' and (a[i][2][0]=='R' or a[i][2][0]=='F'):
-        typeC(a[i])
-    if a[i][0]=='mov' and a[i][1][0]=='R' and a[i][2][0]=='$':
-        typeB(a[i])
+    if a[i][0]=="mov":
+        if len(a[i])==1 or len(a[i])==2:
+            print(str(i+1) + ": wrong instruction given (no operation provided)")
+            quit()
+        elif(a[i][0]=='mov' and a[i][1][0]=='R' and (a[i][2][0]=='R' or a[i][2][0]=='F')):
+            typeC(a[i])
+        elif a[i][0]=='mov' and a[i][1][0]=='R' and a[i][2][0]=='$':
+             typeB(a[i])
+        else:
+            print(str(i+1)+ ": wrong instruction given")
+            quit()
+
     if a[i][0]=='rs' or a[i][0]=='ls':
         typeB(a[i])
     if a[i][0]=='div' or a[i][0]=='not' or a[i][0]=='cmp':
@@ -266,12 +333,13 @@ for i in range (0,len(a)):
     if a[i][0]=='jmp' or a[i][0]=='jlt' or a[i][0]=='jgt' or a[i][0]=='je':
         typeE(a[i])
     if a[i][0]=='hlt':
-        typeF(a[i])
-        
-#below if-else statement is used to call function in which label is read first.
-
+        if len(a[i])!=1:
+            print(str(i+1) + ": Wrong syntax used for instructions")
+            quit()
+        else:
+            typeF(a[i])
     if a[i][0][-1]==':':
-        if a[i][1]=='add' or a[i][1]=='sub' or a[i][1]=='mul':
+        if a[i][1]=='add' or a[i][1]=='sub' or a[i][1]=='mul' or a[i][1]=="and" or a[i][1]=='xor' or a[i][1]=='or':
             typeA(a[i]) 
         if a[i][1]=='div' or a[i][1]=='not' or a[i][1]=='cmp':
             typeC(a[i])
